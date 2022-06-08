cc910aa6b7e9dff0cad4237623499633de8e1d15
Konrad Lewszyk <lewszyk.konrad@gmail.com>
2022-06-08 23:36:02 +0200

    displaying images

--- face_landmarks/rating.py
+++ face_landmarks/rating.py
@@ -1,3 +1,5 @@
+from operator import imod
+from re import sub
 import cv2
 import os
 import math
@@ -15,14 +17,29 @@ from IPython.display import display, Image
 
 from shapely.geometry import Polygon
 
+import subprocess
 
-choice = random.choice(os.listdir("../../faces/high_quality_dataset/hot"))
-choice = "../../faces/high_quality_dataset/hot/" + choice
+import PySimpleGUI as sg
 
-image = cv2.imread(choice)
-plt.imshow(image)
-plt.show()
 
+choices = random.sample(os.listdir("../../faces/high_quality_dataset/hot"), 2)
+choices = ["../../faces/high_quality_dataset/hot/" + choice for choice in choices]
+
+images = [cv2.imread(file) for file in choices]
+# for image in images:
+#     # plt.imshow(image)
+#     # plt.show()
+#     p = subprocess.Popen(["display", image])
+#     # cv2.imshow(image, cv2.IMREAD_ANYCOLOR)
+#     input("do something")
+#     p.kill
+print(choices)
+subprocess.Popen(["display", choices[1]])
+# image = cv2.imread(choice)
+# plt.imshow(image)
+# plt.show()
+
+sg.Window('My window').Layout([[ sg.Image(choices[1]) ]]).Read()
 
 #display(Image(filename = "../../faces/high_quality_dataset/hot/" + choice))
 
