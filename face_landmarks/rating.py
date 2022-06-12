58832c6f3995be75d86d643ab18cd46c33e9a7ab
Konrad Lewszyk <lewszyk.konrad@gmail.com>
2022-06-12 18:22:30 +0200

    moved the code from testing to rating.py, got rid of some unncesessary packages

--- face_landmarks/rating.py
+++ face_landmarks/rating.py
@@ -1,75 +1,98 @@
 from operator import imod
-from re import sub
 import cv2
 import os
 import math
 import dlib
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import imutils
 import random
 from imutils import face_utils
 from scipy.spatial import distance
-
-from PIL import Image
-from IPython.display import display, Image
-
+from PIL import Image, ImageTk
 from shapely.geometry import Polygon
+#package used for GUI
+import tkinter as tk 
 
-import tkinter as tk
+
+#list for storing the ratings user has given
+sequence = []
+
+#a variable used for iteration through different images that will be updated with every click
+value = 0
+
+choices = random.sample(os.listdir("../../faces/high_quality_dataset/hot"), 10)
+choices = ["../../faces/high_quality_dataset/hot/" + choice for choice in choices]
+
+images = [Image.open(x) for x in choices]
+images = [x.resize((400,500)) for x in images]
 
 window = tk.Tk()
 
-collected_values = []
+frame_image = ImageTk.PhotoImage(images.pop())
 
-# def button_clicked():
-#     value = 
+frame_a = tk.Frame()
+frame_b = tk.Frame()
 
-# def increase():
-#     value = int(lbl_value["text"])
-#     lbl_value["text"] = f"{value + 1}"
+def on_click(value):
+    sequence.append(value)
 
-# frame_picture = tk.Frame()
-# frame_rating_buttons = tk.Frame()
+def change_pic():
+    if len(images) == 0:
+        window.destroy()
+    else:
+        photo = ImageTk.PhotoImage(images.pop())
+        picture_placeholder.configure(image=photo)
+        picture_placeholder.image = photo 
+    print("updated")
+
+picture_placeholder = tk.Label(
+    master = frame_a,
+    image = frame_image,
+    text = "Picture",
+    foreground= "white",
+    background="black"
+    )
+picture_placeholder.pack()
+
+button_1 = tk.Button(
+    master = frame_b, 
+    command = lambda: [on_click(1), change_pic()],
+    text = "1",
+    width=25,
+    height=5,
+    foreground="white",
+    background="black")
+button_1.grid(row = 0, column=0)
+
+button_2 = tk.Button(
+    master = frame_b, 
+    command = lambda: [on_click(2), change_pic()],
+    text = "2",
+    width=25,
+    height=5,
+    foreground="white",
+    background="black")
+button_2.grid(row = 0, column=1)
+
+button_3 = tk.Button(
+    master = frame_b, 
+    command = lambda: [on_click(3), change_pic()],
+    text = "3",
+    width=25,
+    height=5,
+    foreground="white",
+    background="black")
+button_3.grid(row = 0, column=2)
+
+frame_b.pack()
+frame_a.pack()
+
+window.mainloop()
 
 
-# lbl_value = tk.Label(master=frame_rating_buttons, text="0")
-# lbl_value.grid(row=0, column=1)
-
-# btn_1 = tk.Button(master=frame_rating_buttons, text="1", command=increase)
-# btn_1.grid(row=0, column=0, sticky="nsew")
-
-# btn_2 = tk.Button(master=frame_rating_buttons, text="2", command=increase)
-# btn_2.grid(row=0, column=1, sticky="nsew")
-
-# btn_3 = tk.Button(master=frame_rating_buttons, text="3", command=increase)
-# btn_3.grid(row=0, column=2, sticky="nsew")
-
-# btn_4 = tk.Button(master=frame_rating_buttons, text="4", command=increase)
-# btn_4.grid(row=0, column=3, sticky="nsew")
-
-# btn_5 = tk.Button(master=frame_rating_buttons, text="5", command=increase)
-# btn_5.grid(row=0, column=4, sticky="nsew")
-
-# frame_picture.pack()
-# frame_rating_buttons.pack()
-
-# window.mainloop()
-#choosing random pictures from a chosen folder
-choices = random.sample(os.listdir("../../faces/high_quality_dataset/hot"), 2)
-choices = ["../../faces/high_quality_dataset/hot/" + choice for choice in choices]
 
 
-# images = [cv2.imread(file) for file in choices]
-# # for image in images:
-# #     # plt.imshow(image)
-# #     # plt.show()
-# #     p = subprocess.Popen(["display", image])
-# #     # cv2.imshow(image, cv2.IMREAD_ANYCOLOR)
-# #     input("do something")
-# #     p.kill
-# print(choices)
 
 
 
