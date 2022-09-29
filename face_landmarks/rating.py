fd23c7d139c775767bbe57a6a8a1283a30e052b0
Konrad Lewszyk <lewszyk.konrad@gmail.com>
2022-09-30 00:20:29 +0200

    added rational rating, fixed flipped order issue

--- face_landmarks/rating.py
+++ face_landmarks/rating.py
@@ -43,17 +43,11 @@ images = [x.resize((400,500)) for x in images]
 def on_click(value):
     sequence.append(value)
 
-#appending values on rational rating
-def evaluate(entry):
-    if 1 <= entry.get() <= 5:
-        sequence.append(eval(entry.get()))
-    else:
-
 def change_pic():
     if len(images) == 0:
         window.destroy()
     else:
-        photo = ImageTk.PhotoImage(images.pop())
+        photo = ImageTk.PhotoImage(images.pop(0))
         picture_placeholder.configure(image=photo)
         picture_placeholder.image = photo 
     print("updated")
@@ -72,121 +66,203 @@ def user_selects_rating_choice():
 rating_choice = user_selects_rating_choice()
 
 
-#Binary
-"""
-window = tk.Tk()
+###################
+#    BINARY       #
+###################
+def binary():
+    window = tk.Tk()
+    frame_image = ImageTk.PhotoImage(images.pop(0))
 
-frame_image = ImageTk.PhotoImage(images.pop())
+    frame_a = tk.Frame()
+    frame_b = tk.Frame()
 
-frame_a = tk.Frame()
-frame_b = tk.Frame()
+    picture_placeholder = tk.Label(
+        master = frame_a,
+        image = frame_image,
+        text = "Picture",
+        foreground= "white",
+        background="black"
+        )
+    picture_placeholder.pack()
+
+    button_1 = tk.Button(
+        master = frame_b, 
+        command = lambda: [on_click(1), change_pic()],
+        text = "1",
+        width=25,
+        height=5,
+        foreground="white",
+        background="black")
+    button_1.grid(row = 0, column=0)
+
+    button_2 = tk.Button(
+        master = frame_b, 
+        command = lambda: [on_click(2), change_pic()],
+        text = "2",
+        width=25,
+        height=5,
+        foreground="white",
+        background="black")
+    button_2.grid(row = 0, column=1)
+
+    frame_b.pack()
+    frame_a.pack()
+    window.mainloop()
+
+    #initiating a dataframe for storing rating results
+    colnames = ['s1', 's2', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
+                    'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19',
+                    'a20', 'a21', 'a22', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+                    'd10', 'd11', 'd12', 'd13', 'd14', 'da1', 'da2']
+    final_dataset = pd.DataFrame(columns = colnames)
+
+    for path in sample:
+        shape = dlib_functions.facial_landmark_processor(path)
+        parameters = dlib_functions.parameter_processor(shape)
+        final_dataset = final_dataset.append(parameters, ignore_index=True)
+
+    final_dataset['image_name'] = image_names
+    final_dataset['rating'] = sequence
+    return(final_dataset)
+
+###################
+#    TRINARY      #
+###################
+def trinary():
+    window = tk.Tk()
+    frame_image = ImageTk.PhotoImage(images.pop(0))
+
+    frame_a = tk.Frame()
+    frame_b = tk.Frame()
+
+    picture_placeholder = tk.Label(
+        master = frame_a,
+        image = frame_image,
+        text = "Picture",
+        foreground= "white",
+        background="black"
+        )
+    picture_placeholder.pack()
+
+    button_1 = tk.Button(
+        master = frame_b, 
+        command = lambda: [on_click(1), change_pic()],
+        text = "1",
+        width=25,
+        height=5,
+        foreground="white",
+        background="black")
+    button_1.grid(row = 0, column=0)
+
+    button_2 = tk.Button(
+        master = frame_b, 
+        command = lambda: [on_click(2), change_pic()],
+        text = "2",
+        width=25,
+        height=5,
+        foreground="white",
+        background="black")
+    button_2.grid(row = 0, column=1)
+
+    button_3 = tk.Button(
+        master = frame_b, 
+        command = lambda: [on_click(3), change_pic()],
+        text = "3",
+        width=25,
+        height=5,
+        foreground="white",
+        background="black")
+    button_3.grid(row = 0, column=2)
+
+    frame_b.pack()
+    frame_a.pack()
+    window.mainloop()
+
+    #initiating a dataframe for storing rating results
+    colnames = ['s1', 's2', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
+                    'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19',
+                    'a20', 'a21', 'a22', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+                    'd10', 'd11', 'd12', 'd13', 'd14', 'da1', 'da2']
+    final_dataset = pd.DataFrame(columns = colnames)
+
+    for path in sample:
+        shape = dlib_functions.facial_landmark_processor(path)
+        parameters = dlib_functions.parameter_processor(shape)
+        final_dataset = final_dataset.append(parameters, ignore_index=True)
+
+    final_dataset['image_name'] = image_names
+    final_dataset['rating'] = sequence
+    return(final_dataset)
+
+###################
+#    RATIONAL      #
+###################
+def rationaL_rating():
+    sequence = []
+
+    sample = random.sample(os.listdir("../../faces/high_quality_dataset/hot"), 5)
+    sample = ["../../faces/high_quality_dataset/hot/" + choice for choice in sample]
+
+    image_names = [re.search(r"image_\d+.jpg", path).group() for path in sample]
+
+    images = [Image.open(x) for x in sample]
+    images = [x.resize((400,500)) for x in images]
+
+    def change_picture_and_store_rating(event): 
+        if 1 <= eval(entry.get()) <= 5:
+            res.configure(text = "Result: " + str(eval(entry.get())))
+            sequence.append(eval(entry.get()))
+            if len(images) == 0:
+                window.destroy()
+            else:
+                photo = ImageTk.PhotoImage(images.pop(0))
+                picture_placeholder.configure(image=photo)
+                picture_placeholder.image = photo 
+        
+    window = tk.Tk()
+    frame_a = tk.Frame()
+    frame_image = ImageTk.PhotoImage(images.pop(0))
+
+    picture_placeholder = tk.Label(
+        master = frame_a,
+        image = frame_image,
+        text = "Picture",
+        foreground= "white",
+        background="black"
+        )
+    picture_placeholder.pack()
+
+    tk.Label(window, text="You are rating in rational numbers from 1 to 5 (you can input digits after the comma):").pack()
+
+    entry = tk.Entry(window)
+    entry.bind("<Return>", change_picture_and_store_rating)
+
+    frame_a.pack()
+    entry.pack()
+    res = tk.Label(window)
+    res.pack()
+
+    window.mainloop()
+
+    #initiating a dataframe for storing rating results
+    colnames = ['s1', 's2', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
+                    'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19',
+                    'a20', 'a21', 'a22', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+                    'd10', 'd11', 'd12', 'd13', 'd14', 'da1', 'da2']
+
+    final_dataset = pd.DataFrame(columns = colnames)
+
+    for path in sample:
+        shape = dlib_functions.facial_landmark_processor(path)
+        parameters = dlib_functions.parameter_processor(shape)
+        final_dataset = final_dataset.append(parameters, ignore_index=True)
+
+    final_dataset['image_name'] = image_names
+    final_dataset['rating'] = sequence
+
+    return(final_dataset)
 
 
-picture_placeholder = tk.Label(
-    master = frame_a,
-    image = frame_image,
-    text = "Picture",
-    foreground= "white",
-    background="black"
-    )
-picture_placeholder.pack()
-
-button_1 = tk.Button(
-    master = frame_b, 
-    command = lambda: [on_click(1), change_pic()],
-    text = "1",
-    width=25,
-    height=5,
-    foreground="white",
-    background="black")
-button_1.grid(row = 0, column=0)
-
-button_2 = tk.Button(
-    master = frame_b, 
-    command = lambda: [on_click(2), change_pic()],
-    text = "2",
-    width=25,
-    height=5,
-    foreground="white",
-    background="black")
-button_2.grid(row = 0, column=1)
-
-button_3 = tk.Button(
-    master = frame_b, 
-    command = lambda: [on_click(3), change_pic()],
-    text = "3",
-    width=25,
-    height=5,
-    foreground="white",
-    background="black")
-button_3.grid(row = 0, column=2)
-
-frame_b.pack()
-frame_a.pack()
-
-window.mainloop()
-
-"""
-
-#RATIONAL
-window = tk.Tk()
-
-frame_image = ImageTk.PhotoImage(images.pop())
-
-frame_a = tk.Frame()
-number_entry = tk.Entry()
-frame_b = tk.Frame()
-
-
-picture_placeholder = tk.Label(
-    master = frame_a,
-    image = frame_image,
-    text = "Picture",
-    foreground= "white",
-    background="black"
-    )
-picture_placeholder.pack()
-
-button_1 = tk.Button(
-    master = frame_b, 
-    command = lambda: [on_click(1), change_pic()],
-    text = "SUBMIT",
-    width=25,
-    height=5,
-    foreground="white",
-    background="black")
-button_1.grid(row = 0, column=0)
-
-
-
-frame_b.pack()
-number_entry.pack()
-frame_a.pack()
-
-
-window.mainloop()
-
-
-print(len(sequence))
-print(sequence)
-print(image_names)
-
-#initiating a dataframe for storing rating results
-colnames = ['s1', 's2', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
-                'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19',
-                'a20', 'a21', 'a22', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
-                'd10', 'd11', 'd12', 'd13', 'd14', 'da1', 'da2']
-
-final_dataset = pd.DataFrame(columns = colnames)
-
-for path in sample:
-    shape = dlib_functions.facial_landmark_processor(path)
-    parameters = dlib_functions.parameter_processor(shape)
-    final_dataset = final_dataset.append(parameters, ignore_index=True)
-
-final_dataset['image_name'] = image_names
-final_dataset['rating'] = sequence
-print(final_dataset)
 
 filename = input("please enter your first and last name in this format: firstname_lastname  ")
 filename = "datasets/" + filename + ".csv"
